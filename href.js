65b4b04a17c0cbbb9a2f345c163b22f7388bd7e0
Iyanuoluwa fanoro <iycodes@outlook.com>
2024-02-28 09:08:46 +0100

    dd

---
+++ href.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.links;
+  console.log(links);
+
+  for (let i = 0; i < links.length; i++) {
+    const element = links[i];
+    element.classList.add("interact-button");
+    element.setAttribute("href", "javascript:void()");
+    // element.addEventListener("click", () => {
+    //   ace();
+    // });
+  }
+
+  const buttons = document.getElementsByTagName("button");
+  for (let i = 0; i < buttons.length; i++) {
+    const element = buttons[i];
+    element.classList.add("interact-button");
+    // element.addEventListener("click", () => {
+    //   ace();
+    // });
+  }
+});
